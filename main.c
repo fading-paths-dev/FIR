65ac7133aa8d07d24e9cf3b538c603f124cacb73
Kathryn Whitehead <Kathryn.Whitehead@apollo-fire.com>
2022-10-06 14:05:36 +0100

    Floats to integers x1000

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include <msp430.h> 
 #include <stdint.h>
 #include <float.h>
+#include <stdio.h>
 
 #define ELEMENTS  10
-static float coefficients_array[ELEMENTS] =  {0.011063, 0.031347, 0.087738, 0.159724, 0.210128, 0.210128, 0.159724, 0.087738, 0.031347, 0.011063};
-static float filter_array[ELEMENTS];
+//static float coefficients_array[ELEMENTS] =  {0.011063, 0.031347, 0.087738, 0.159724, 0.210128, 0.210128, 0.159724, 0.087738, 0.031347, 0.011063};
+static uint8_t coefficients_array[ELEMENTS] =  {11, 31, 88, 160, 210, 210, 160, 88, 33, 11};
+static int32_t filter_array[ELEMENTS];
+static float input_array[ELEMENTS * 10U];
+static float output_array[ELEMENTS * 10U];
 
-static float calculateFilterOutput (float input);
+static float calculateFilterOutput (int32_t input);
 
-static float calculateFilterOutput (float input)
+static float calculateFilterOutput (int32_t input)
 {
     float sum = 0;
     for (uint8_t i = (ELEMENTS - 1); i > 0; i--)
@@ -20,7 +24,7 @@ static float calculateFilterOutput (float input)
     {
         sum += (filter_array[p] * coefficients_array[p]);
     }
-    return sum;
+    return (sum / 1000000.0);
 }
 
 /**
@@ -30,5 +34,11 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
+	for(uint8_t i = 0; i < (ELEMENTS * 10U); i++)
+	{
+	    output_array[i] = calculateFilterOutput ((int32_t) (input_array[i] * 1000.0));
+	}
+
+
 	return 0;
 }
